bdcfa4bbf72ef465ab7ab8f2d3b8ff50a01aeecd
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2017-12-15 11:34:05 +0200

    Add gitignore and add new file

---
+++ my-if.scm
@@ -0,0 +1,26 @@
+#lang scheme
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.01))
+
+(define (improve guess x)
+  (average  guess (/ x guess)))
+
+(define (my-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+;; here infinite recursion 
+(define (sqrt-iter guess x)
+  (my-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
