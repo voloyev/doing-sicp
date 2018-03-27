3d602ec91cd584e3c51de1fd75ea32b1aaed6e1f
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-03-27 16:34:17 +0300

    Add infinite loop

--- square-newton.scm
+++ square-newton.scm
@@ -2,6 +2,10 @@
 (define (square x)
   (* x x))
 
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
 
@@ -9,7 +13,12 @@
   (/ (+ x y) 2))
 
 (define (improve guess x)
-  (aveage guess (/ x guess)))
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
