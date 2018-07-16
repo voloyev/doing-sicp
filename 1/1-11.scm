bdc87d124d0baa397d2c13620275fbd2800aa147
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-07-16 15:25:58 +0300

    Add comments

--- 1/1-11.scm
+++ 1/1-11.scm
@@ -1,4 +1,8 @@
+;; recursive process
 (define (f n)
   (if (< n 3)
       n
       (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+;; iterative process
+(define (f a b counter)
+  counter) ;; TODO
